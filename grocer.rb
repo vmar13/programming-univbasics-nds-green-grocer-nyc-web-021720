f8e65c8f1c62b1e8ec251f4a118f85355ff33798
Vanessa Martinez <vmar76@gmail.com>
2020-02-14 07:05:00 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -21,7 +21,7 @@ def consolidate_cart(cart)
     new_cart_item = {
       :item => cart[counter][:item],
       :price => cart[counter][:price],
-      :clearance => [counter][:clearance],
+      :clearance => cart[counter][:clearance],
       :count => 1 
     }
     new_cart << new_cart_item
